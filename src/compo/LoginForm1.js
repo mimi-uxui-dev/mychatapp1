b2321ce1a5ca412a07353653ce21d644b93c5f0c
mimi-uxui-dev <uxui.mimi@gmail.com>
2021-04-03 22:40:30 +0100

    Final V0

--- src/compo/LoginForm1.js
+++ src/compo/LoginForm1.js
@@ -24,7 +24,7 @@ const LoginForm = () => {
       setError('');
     
     } catch (err) {
-      setError('Oops, incorrect credentials.');
+      setError('Oops, incorrect credentials.', err);
     }
 
   };
